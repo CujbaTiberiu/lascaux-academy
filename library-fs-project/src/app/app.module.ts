71ded51c7436fbdc2ea454798bef6a2f33591e7b
tiby <cujba.tiberiu@gmail.com>
2024-11-19 17:46:42 +0100

    fix imports module

--- library-fs-project/src/app/app.module.ts
+++ library-fs-project/src/app/app.module.ts
@@ -5,15 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {SharedModule} from './shared/shared.module';
-import { DashboardComponent } from './features/main/admin/dashboard/dashboard.component';
-import { PageComponent } from './features/main/user/page/page.component';
 import {provideHttpClient} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DashboardComponent,
-    PageComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
